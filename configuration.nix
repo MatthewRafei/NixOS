e47811e0685ef31d8ec1300f08158d21f075a0c8
MatthewRafei <matthewrafei1@gmail.com>
2024-04-23 12:52:26 -0500

    Added auto update

--- configuration.nix
+++ configuration.nix
@@ -179,6 +179,10 @@
   #networking.firewall.allowedUDPPorts = [ 5986; ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
+  
+
+
+  system.autoUpgrade.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
